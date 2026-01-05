517639b8705d60f818acdfbbe87e4fab74337795
chan <pcr0807@naver.com>
2026-01-05 22:26:49 +0900

    Payment Setting

---
+++ apps/payment/src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import Joi from 'joi';
+import { PaymentModule } from './payment/payment.module';
+
+@Module({
+  imports: [
+    PaymentModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DB_URL: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.getOrThrow('DB_URL'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class AppModule {}
---
+++ apps/payment/src/payment/entity/payment.entity.ts
@@ -0,0 +1,55 @@
+import { Column, Entity } from 'typeorm';
+
+export enum PaymentStatus {
+  pending = 'Pending',
+  rejected = 'Rejected',
+  approved = 'Approved',
+}
+
+export enum PaymentMethod {
+  creditCard = 'CreditCard',
+  kakao = 'Kakao',
+}
+
+export enum NotificationStatus {
+  pending = 'Pending',
+  sent = 'Sent',
+}
+
+@Entity()
+export class Payment {
+  id: string;
+
+  @Column({
+    enum: PaymentStatus,
+    default: PaymentStatus.pending,
+  })
+  paymentStatus: PaymentStatus;
+
+  @Column({
+    enum: PaymentMethod,
+    default: PaymentMethod.creditCard,
+  })
+  paymentMethod: PaymentMethod;
+
+  @Column()
+  cardNumber: string;
+
+  @Column()
+  expireYear: string;
+
+  @Column()
+  expireMonth: string;
+
+  @Column()
+  birthOrRegistration: string;
+
+  @Column()
+  passwordTwoDigits: string;
+
+  @Column({
+    enum: NotificationStatus,
+    default: NotificationStatus.pending,
+  })
+  notificationStatus: NotificationStatus;
+}
