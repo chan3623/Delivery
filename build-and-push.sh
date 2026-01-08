22fbddb9dedf78e0d8fad601a17f6b86018cf8e8
chan <pcr0807@naver.com>
2026-01-08 14:58:03 +0900

    배포 Github CICD 생성 5

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-docker build build --platform linux/amd64, linux/arm64 -t chn3623/nestjs-gateway:latest -f ./apps/gateway/Dockerfile --target product .
-docker build build --platform linux/amd64, linux/arm64 -t chn3623/nestjs-notification:latest -f ./apps/notification/Dockerfile --target product .
-docker build build --platform linux/amd64, linux/arm64 -t chn3623/nestjs-order:latest -f ./apps/order/Dockerfile --target product .
-docker build build --platform linux/amd64, linux/arm64 -t chn3623/nestjs-payment:latest -f ./apps/payment/Dockerfile --target product .
-docker build build --platform linux/amd64, linux/arm64 -t chn3623/nestjs-product:latest -f ./apps/product/Dockerfile --target product .
-docker build build --platform linux/amd64, linux/arm64 -t chn3623/nestjs-user:latest -f ./apps/user/Dockerfile --target product .
+docker build build --platform linux/amd64, linux/arm64 -t chn3623/nestjs-gateway:latest -f ./apps/gateway/Dockerfile --target production .
+docker build build --platform linux/amd64, linux/arm64 -t chn3623/nestjs-notification:latest -f ./apps/notification/Dockerfile --target production .
+docker build build --platform linux/amd64, linux/arm64 -t chn3623/nestjs-order:latest -f ./apps/order/Dockerfile --target production .
+docker build build --platform linux/amd64, linux/arm64 -t chn3623/nestjs-payment:latest -f ./apps/payment/Dockerfile --target production .
+docker build build --platform linux/amd64, linux/arm64 -t chn3623/nestjs-product:latest -f ./apps/product/Dockerfile --target production .
+docker build build --platform linux/amd64, linux/arm64 -t chn3623/nestjs-user:latest -f ./apps/user/Dockerfile --target production .
 
 docker push chn3623/nestjs-gateway:latest
 docker push chn3623/nestjs-notification:latest
