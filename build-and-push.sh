67ce3ce566d5e3ab8db07bd824106c9951558a1a
chan <pcr0807@naver.com>
2026-01-08 15:17:27 +0900

    build shell 수정

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-docker build build --platform linux/amd64, linux/arm64 -t chn3623/nestjs-gateway:latest -f ./apps/gateway/Dockerfile --target production .
-docker build build --platform linux/amd64, linux/arm64 -t chn3623/nestjs-notification:latest -f ./apps/notification/Dockerfile --target production .
-docker build build --platform linux/amd64, linux/arm64 -t chn3623/nestjs-order:latest -f ./apps/order/Dockerfile --target production .
-docker build build --platform linux/amd64, linux/arm64 -t chn3623/nestjs-payment:latest -f ./apps/payment/Dockerfile --target production .
-docker build build --platform linux/amd64, linux/arm64 -t chn3623/nestjs-product:latest -f ./apps/product/Dockerfile --target production .
-docker build build --platform linux/amd64, linux/arm64 -t chn3623/nestjs-user:latest -f ./apps/user/Dockerfile --target production .
+docker buildx build --platform linux/amd64, linux/arm64 -t chn3623/nestjs-gateway:latest -f ./apps/gateway/Dockerfile --target production .
+docker buildx build --platform linux/amd64, linux/arm64 -t chn3623/nestjs-notification:latest -f ./apps/notification/Dockerfile --target production .
+docker buildx build --platform linux/amd64, linux/arm64 -t chn3623/nestjs-order:latest -f ./apps/order/Dockerfile --target production .
+docker buildx build --platform linux/amd64, linux/arm64 -t chn3623/nestjs-payment:latest -f ./apps/payment/Dockerfile --target production .
+docker buildx build --platform linux/amd64, linux/arm64 -t chn3623/nestjs-product:latest -f ./apps/product/Dockerfile --target production .
+docker buildx build --platform linux/amd64, linux/arm64 -t chn3623/nestjs-user:latest -f ./apps/user/Dockerfile --target production .
 
 docker push chn3623/nestjs-gateway:latest
 docker push chn3623/nestjs-notification:latest
